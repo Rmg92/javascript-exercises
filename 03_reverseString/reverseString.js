09c528c7e81367d76ff36e22f1239dfccb328efb
Ricardo <ricardomg92@hotmail.com>
2021-11-17 21:30:50 +0100

    completed exercice 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
 const reverseString = function(str) {
-
+    const inputString = str;
+    let reversedString = '';
+    for (let i = 0; i <= inputString.length; i++) {
+        reversedString += inputString.charAt(inputString.length - i);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
