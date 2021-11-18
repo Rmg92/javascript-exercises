997393e608f470558c6692776745b43b455acecd
Ricardo <ricardomg92@hotmail.com>
2021-11-18 21:51:43 +0100

    Completed Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...remove) {
+    for (let j = 0; j < remove.length; j++) {
+        for (let i = 0; i <= arr.length; i++) {
+            if (arr[i] === remove[j]) {               
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
