d945f7fdc42b72f16afe327fbeb7370776371a26
Ricardo <ricardomg92@hotmail.com>
2021-11-20 18:03:01 +0100

    Completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  let celsius = Math.round(((temp - 32) * (5 / 9))*10)/10;
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  let fahrenheit = Math.round((temp * (9/5) + 32)*10)/10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
